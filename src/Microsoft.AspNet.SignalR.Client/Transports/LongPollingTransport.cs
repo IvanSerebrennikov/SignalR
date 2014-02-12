cba39d277f6c1011b3f6e414d4954502626430cc
Stephen Halter <halter73@gmail.com>
2014-02-11 16:52:25 -0800

    Disable long-polling keep-alive checks in the .NET client for old servers
    - We determine that the server doesn't send long-polling keep-alives
      if it doesn't send down a LongPollDelay value on negotiate.

--- src/Microsoft.AspNet.SignalR.Client/Transports/LongPollingTransport.cs
+++ src/Microsoft.AspNet.SignalR.Client/Transports/LongPollingTransport.cs
@@ -43,7 +43,8 @@ namespace Microsoft.AspNet.SignalR.Client.Transports
         {
             get
             {
-                return true;
+                return NegotiationResponse != null &&
+                       NegotiationResponse.LongPollDelay.HasValue;
             }
         }
 
